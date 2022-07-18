c8901b2c42d687d52b0cd487496e773469520b87
Josh Reid <jsreid13@gmail.com>
2022-07-18 02:51:58 +0000

    Initial commit for week4 assignment

---
+++ week4/assignment.sh
@@ -0,0 +1,2 @@
+./delete-indexes.sh
+./index-data.sh -r -y /workspace/search_with_machine_learning_course/week4/utilities -p /workspace/search_with_machine_learning_course/week4/conf/bbuy_products.json
--- week4/utilities/index_products.py
+++ week4/utilities/index_products.py
@@ -15,6 +15,7 @@ import requests
 import json
 
 from time import perf_counter
+from sentence_transformers import SentenceTransformer, util
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -107,6 +108,7 @@ def get_opensearch():
 def index_file(file, index_name, reduced=False):
     logger.info("Creating Model")
     # IMPLEMENT ME: instantiate the sentence transformer model!
+    model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
     
     logger.info("Ready to index")
 
@@ -136,10 +138,15 @@ def index_file(file, index_name, reduced=False):
             continue
         if reduced and ('categoryPath' not in doc or 'Best Buy' not in doc['categoryPath'] or 'Movies & Music' in doc['categoryPath']):
             continue
+        names.append(doc['name'])
         docs.append({'_index': index_name, '_id':doc['sku'][0], '_source' : doc})
         #docs.append({'_index': index_name, '_source': doc})
         docs_indexed += 1
         if docs_indexed % 200 == 0:
+            logger.info("Encoding")
+            embeddings = model.encode(names)
+            for doc, embedding in zip(docs, embeddings):
+                doc['_source']['embedding'] = embedding
             logger.info("Indexing")
             bulk(client, docs, request_timeout=60)
             logger.info(f'{docs_indexed} documents indexed')
